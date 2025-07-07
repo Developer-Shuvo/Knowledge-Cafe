710b8fbe4ff70f9b8f0084940b2745c6522dc99a
SHUVO <im.computer.engineeer@gamil.com>
2025-07-07 10:36:23 +0600

    Fake Data Create and connect

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+             <h2 className="text-6xl font-extrabold text-blue-800">Knowledge Cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
